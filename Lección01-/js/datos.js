6c7490cf82c683495d0e65bd47ed1bfd3d0b98c6
Ari Abril <occamppo.3025@gmail.com>
2022-02-15 14:15:06 -0600

    add files

---
+++ Lección01-/js/datos.js
@@ -0,0 +1,17 @@
+// Tipos de datos en JavaScript
+
+//Cadena
+let nombre = "Abril"; 
+console.log(nombre);
+
+// Numérico
+let numero = 280699;
+console.log(numero);
+
+//Objeto 
+let objeto = {
+    nombre : "",
+    apellido : "",
+    edad : ""
+};
+console.log(objeto);
